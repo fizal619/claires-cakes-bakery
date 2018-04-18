bf48955f6df14e139d6673e6ce37afe861145370
maiya-22 <maiya_o@yahoo.com>
2018-04-18 16:57:01 -0400

    json parsing in front-end, one level deep

--- app.rb
+++ app.rb
@@ -4,8 +4,16 @@ require 'json'
 # include SenGrid
 require_relative './products/Products.rb'
 
+# p $all_products
 
-p $all_products
+
+$sample_data = {
+    name: "blue doughnut",
+    title: "blue doughnut title",
+    price: 1
+}
+
+
 
 get "/" do
     # "<p>root route</p>"
@@ -19,7 +27,13 @@ get "/test" do
     # "<p>test route</p>"
     content_type :json
     # { :key1 => 'value1', :key2 => 'value2' }.to_json
-    $all_products.to_json
+    #  $all_products.to_json
+    #  $all_products = JSON.recurse_proc($all_products)
+
+    # $all_products = JSON.load(str, nil, create_additions: true)
+
+    # $all_products
+    $sample_data.to_json
 end
 
 get "/email" do 
--- public/scripts/request-helpers.js
+++ public/scripts/request-helpers.js
@@ -1,25 +1,32 @@
-console.log('request helpers is loaded');
+console.log("request helpers is loaded");
 
 // make a helper function that returns a promise for any fetch to backend routes:
 // default request type is 'GET'
-function requestPromise(uri, method = 'GET') {
+function requestPromise(uri, method = "GET") {
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, uri);
     xhr.send();
-    xhr.onload = function () {
+    xhr.onload = function() {
       resolve(JSON.stringify(xhr.responseText));
     };
-    xhr.onerror = function () {
+    xhr.onerror = function() {
       reject(xhr.statusText);
     };
   });
 }
-const request = requestPromise('http://localhost:4567/test', 'GET');
+const request = requestPromise("http://localhost:4567/test", "GET");
 request
-  .then((data) => {
-    console.log(`data in the response: ${data}`);
+  .then(data => {
+    console.log(typeof data);
+    // let div = document.getElementById("test");
+
+    data = JSON.parse(JSON.parse(data));
+
+    console.log(typeof data);
+    console.log(Object.keys(data));
+    div.innerHTML = data;
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(`error in the response: ${err}`);
   });
